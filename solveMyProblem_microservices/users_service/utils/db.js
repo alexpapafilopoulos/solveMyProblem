4c8f173aa1c0fa4ef6bebac4de6ab94ff6d54c6e
her_username <papafilopoulosa@gmail.com>
2024-04-09 02:03:20 +0300

    testing git

--- solveMyProblem_microservices/users_service/utils/db.js
+++ solveMyProblem_microservices/users_service/utils/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const DB_CONFIG = require("./db_config.js"); // Corrected path
-//testing git
+//testing git again
 const sequelize = new Sequelize(
     DB_CONFIG.DB,
     DB_CONFIG.USER,
